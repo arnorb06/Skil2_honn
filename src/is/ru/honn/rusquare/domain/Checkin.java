f7e5d85097e90010d1d3bda27cbc899bd21acde3
Arnór Barkarson <arnorb06@ru.is>
2010-11-05 14:31:53 +0000

    Checkin added

---
+++ src/is/ru/honn/rusquare/domain/Checkin.java
@@ -0,0 +1,16 @@
+/**
+ * @Skilaverkefni2 Nov 5, 2010 Arn—r Barkarson
+ *
+ * Copyright (c) Arn—r Barkarson
+ */
+package is.ru.honn.rusquare.domain;
+
+/**
+ * @author Arn—r Barkarson
+ *
+ * @version 1.0, Nov 5, 2010
+ */
+public class Checkin {
+	String username;
+	String venuename;
+}
