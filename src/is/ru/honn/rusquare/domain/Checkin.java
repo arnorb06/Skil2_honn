c3252d1270287f73f4a9e36423138062e3192456
Arnór Barkarson <arnorb06@ru.is>
2010-11-06 11:25:35 +0000

    Breytum og get/set bætt í checkin

--- src/is/ru/honn/rusquare/domain/Checkin.java
+++ src/is/ru/honn/rusquare/domain/Checkin.java
@@ -5,12 +5,93 @@
  */
 package is.ru.honn.rusquare.domain;
 
+import java.util.Date;
+
 /**
  * @author Arn—r Barkarson
  *
  * @version 1.0, Nov 5, 2010
  */
 public class Checkin {
+	int id;
+	String title;
+	Date date;
 	String username;
-	String venuename;
+	int venueId;
+	/**
+	 * @param id
+	 * @param title
+	 * @param date
+	 * @param username
+	 * @param venueId
+	 */
+	public Checkin(int id, String title, Date date, String username, int venueId) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.date = date;
+		this.username = username;
+		this.venueId = venueId;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the venueId
+	 */
+	public int getVenueId() {
+		return venueId;
+	}
+	/**
+	 * @param venueId the venueId to set
+	 */
+	public void setVenueId(int venueId) {
+		this.venueId = venueId;
+	}
+	
 }
