7d9cd5048fcde16f150e88e44bb84cf287e0d54b
Thordur Arnarson <thordurba08@ru.is>
2010-11-06 13:50:53 +0000

    added CheckinServiceImpl

---
+++ src/is/ru/honn/rusquare/service/CheckinServiceImpl.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)Skil2_honn Nov 6, 2010 Thordur Arnarson
+ *
+ * Copyright (c) Thordur Arnarson
+ */
+package is.ru.honn.rusquare.service;
+
+import is.ru.honn.rusquare.domain.Checkin;
+
+/**
+ * @author Thordur Arnarson
+ * @version 1.0 Nov 6, 2010
+ */
+public class CheckinServiceImpl implements CheckinService{
+
+	/* (non-Javadoc)
+	 * @see is.ru.honn.rusquare.service.CheckinService#checkin(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Checkin checkin(String username, String venuename) {
+		
+		return null;
+	}
+
+}
