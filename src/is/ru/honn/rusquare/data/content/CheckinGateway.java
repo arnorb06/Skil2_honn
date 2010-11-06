52975100c77325426627283ef84c03b15fc1fd17
Thordur Arnarson <thordurba08@ru.is>
2010-11-06 10:56:05 +0000

    added CheckinGateway and DataGateway

---
+++ src/is/ru/honn/rusquare/data/content/CheckinGateway.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)Skil2_honn Nov 6, 2010 Thordur Arnarson
+ *
+ * Copyright (c) Thordur Arnarson
+ */
+package is.ru.honn.rusquare.data.content;
+
+import is.ruframework.data.RuGenericDataAccess;
+
+/**
+ * @author Thordur Arnarson
+ * @version 1.0 Nov 6, 2010
+ */
+public class CheckinGateway extends RuGenericDataAccess implements DataGateway {
+
+	/* (non-Javadoc)
+	 * @see is.ru.honn.rusquare.data.content.DataGateway#add()
+	 */
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see is.ru.honn.rusquare.data.content.DataGateway#getContent()
+	 */
+	@Override
+	public void getContent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/is/ru/honn/rusquare/data/content/DataGateway.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)Skil2_honn Nov 6, 2010 Thordur Arnarson
+ *
+ * Copyright (c) Thordur Arnarson
+ */
+package is.ru.honn.rusquare.data.content;
+
+/**
+ * @author Thordur Arnarson
+ * @version 1.0 Nov 6, 2010
+ */
+public interface DataGateway {
+	public void add();
+	public void getContent();
+}
