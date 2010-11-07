19675dc324bb20c3ffcfd4a11937ecd066e94626
Thordur Arnarson <thordurba08@ru.is>
2010-11-07 16:04:44 +0000

    added VenueServiceImpl

---
+++ src/is/ru/honn/rusquare/service/VenueServiceImpl.java
@@ -0,0 +1,14 @@
+/**
+ * @(#)Skil2_honn Nov 7, 2010 Thordur Arnarson
+ *
+ * Copyright (c) Thordur Arnarson
+ */
+package is.ru.honn.rusquare.service;
+
+/**
+ * @author Thordur Arnarson
+ * @version 1.0 Nov 7, 2010
+ */
+public class VenueServiceImpl {
+
+}
