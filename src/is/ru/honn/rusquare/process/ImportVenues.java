1d0f60fb9413b9204f11d88603612185d10a6c1c
Arnór Barkarson <arnorb06@ru.is>
2010-11-08 15:12:05 +0000

    bætti við í app.xml og bjóttil testfyrir dæmi 3

--- src/is/ru/honn/rusquare/process/ImportVenues.java
+++ src/is/ru/honn/rusquare/process/ImportVenues.java
@@ -5,7 +5,14 @@
  */
 package is.ru.honn.rusquare.process;
 
+import is.ru.honn.rusquare.service.VenueServiceImpl;
+import is.ruframework.feeds.RuFeedReader;
 import is.ruframework.process.RuAbstractProcess;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * @author Arn—r Barkarson
@@ -14,15 +21,31 @@ import is.ruframework.process.RuAbstractProcess;
  */
 public class ImportVenues extends RuAbstractProcess {
 	
+	Logger log = Logger.getLogger(this.getClass().getName());
+	VenueServiceImpl venueService;
+	RuFeedReader reader;
 	
+	
+	public void beforeProsess(){
+		log.info("before prosess");
+		ApplicationContext appCtx = new FileSystemXmlApplicationContext("app.xml");
+		
+		reader =  
+		//ApplicationContext venueCtx = new FileSystemXmlApplicationContext("venues.xml");
+		
+		venueService = (VenueServiceImpl)appCtx.getBean("VenueServiceImpl");
+	}
 	
 	/* (non-Javadoc)
 	 * @see is.ruframework.process.RuAbstractProcess#startProcess()
 	 */
 	@Override
 	public void startProcess() {
-		// TODO Auto-generated method stub
-
+		log.info("start prosess");
+	}
+	
+	public void afterProsess(){
+		log.info("after process");
 	}
 
 }
---
+++ src/is/ru/honn/rusquare/test/test_3.java
@@ -0,0 +1,27 @@
+/**
+ * @Skilaverkefni2 Nov 8, 2010 Arn—r Barkarson
+ *
+ * Copyright (c) Arn—r Barkarson
+ */
+package is.ru.honn.rusquare.test;
+
+import is.ru.honn.rusquare.process.ImportVenues;
+
+/**
+ * @author Arn—r Barkarson
+ *
+ * @version 1.0, Nov 8, 2010
+ */
+public class test_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ImportVenues importVenue = new ImportVenues();
+		
+		importVenue.beforeProsess();
+	}
+
+}
